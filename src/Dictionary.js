fd54a86603fb9cad252c91e8481915a7ebfdaf66
steph-sketch850 <145380074+steph-sketch850@users.noreply.github.com>
2024-09-02 14:54:12 -0400

    Added API and imported axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
@@ -6,6 +7,15 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+  }
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+  function search(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
