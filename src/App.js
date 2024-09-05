43c5115022c56561ae3e6a0cbb8a9450230c2874
steph-sketch850 <145380074+steph-sketch850@users.noreply.github.com>
2024-09-05 14:50:54 -0400

    Imported bootstrap for photos and changed footer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,19 @@ export default function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Stephanie Rodriguez</small>
+          <small>
+            This project was coded by{" "}
+            <a href="https://github.com/steph-sketch850">Stephanie Rodriguez</a>{" "}
+            and is{" "}
+            <a href="https://github.com/steph-sketch850/react-dictionary-project">
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a href="https://webster-dictionary.netlify.app/">
+              hosted on Netlify
+            </a>
+            .
+          </small>
         </footer>
       </div>
     </div>
