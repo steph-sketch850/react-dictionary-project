108c0a049c2fd2cfe5cc7108c55d2fe28cb78faa
steph-sketch850 <145380074+steph-sketch850@users.noreply.github.com>
2024-09-05 14:03:49 -0400

    Created styling to project

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">Dictionary App</header>
         <main>
           <Dictionary />
         </main>
